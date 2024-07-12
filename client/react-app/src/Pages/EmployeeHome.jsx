153c90e9e2332ce6825ff29e1feea5e2aa432965
Sean Vargas <seanva@me.com>
2024-07-12 14:47:47 -0400

    Fetch feedback and questions

--- client/react-app/src/Pages/EmployeeHome.jsx
+++ client/react-app/src/Pages/EmployeeHome.jsx
@@ -3,22 +3,38 @@ import FeedbackForm from '../Components/FeedbackForm';
 
 function EmployeeHome() {
   const [feedback, setFeedback] = useState([]);
+  const [questions, setQuestions] = useState([]);
 
-  const fetchFeedback = () => {
-    fetch("http://localhost:3000/feedback")
-      .then((res) => res.json())
-      .then((feedback) => {
-        setFeedback(feedback);
-      });
-    console.log(feedback);
-  };
-  useEffect(fetchFeedback, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const feedbackResponse = await fetch("http://localhost:3000/feedback");
+        if (!feedbackResponse.ok) {
+          throw new Error("Feedback could not be fetched")
+        }
+        const feedbackResponseJson = await feedbackResponse.json();
+        setFeedback(feedbackResponseJson);
+        console.log(feedback);
+
+        const questionsResponse = await fetch("http://localhost:3000/questions");
+        if (!questionsResponse.ok) {
+          throw new Error("Questions could not be fetched")
+        }
+        const questionsResponseJson = await questionsResponse.json();
+        setQuestions(questionsResponseJson);
+        console.log(questions);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Employee Dashboard</h1>
       <h3>Your manager is: </h3>
-      <FeedbackForm />
+      <FeedbackForm fb={feedback} qns={questions}/>
 
       <h2>Past Submitted responses</h2>
     </div>
