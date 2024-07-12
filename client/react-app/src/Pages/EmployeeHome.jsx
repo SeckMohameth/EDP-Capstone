0e2236462e1d4ce6f00bc73457631e3ad70e17a5
Sean Vargas <seanva@me.com>
2024-07-12 13:45:51 -0400

    Create logic to fetch feedback in employee component

--- client/react-app/src/Pages/EmployeeHome.jsx
+++ client/react-app/src/Pages/EmployeeHome.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 function EmployeeHome() {
+  const [feedback, setFeedback] = useState([]);
+
+  const fetchFeedback = () => {
+    fetch("http://localhost:3000/feedback")
+      .then((res) => res.json())
+      .then((feedback) => {
+        setFeedback(feedback);
+      });
+    console.log(feedback);
+  };
+  useEffect(fetchFeedback, []);
+
   return (
     <div>
       <h1>Employee Dashboard</h1>
