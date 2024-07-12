25f278d60577ddeca6f982ee57dae88ee2702966
Mohameth Seck <mohamethseck34@gmail.com>
2024-07-12 14:15:22 -0400

    fixed missing useEffect error

--- client/react-app/src/Pages/EmployeeHome.jsx
+++ client/react-app/src/Pages/EmployeeHome.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FeedbackForm from '../Components/FeedbackForm'
 import QuestionFrom from '../Components/QuestionForm'
 
