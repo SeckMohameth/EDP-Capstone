f4e3e7fde066a4dbbaf138e3885e3a268295296c
Sean Vargas <seanva@me.com>
2024-07-12 13:35:08 -0400

    Create logic to add new employee

--- client/react-app/src/Pages/EmployeeHome.jsx
+++ client/react-app/src/Pages/EmployeeHome.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function EmployeeHome() {
   return (
     <div>
       <h1>Employee Dashboard</h1>
     </div>
-  )
+  );
 }
 
-export default EmployeeHome
+export default EmployeeHome;
