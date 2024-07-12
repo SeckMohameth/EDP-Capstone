9e5c4bcb68e75aa875014e654d9e7b297bc0fa7b
Mohameth Seck <mohamethseck34@gmail.com>
2024-07-12 11:50:25 -0400

    auth forms done

--- client/react-app/src/Pages/Signup.jsx
+++ client/react-app/src/Pages/Signup.jsx
@@ -5,12 +5,22 @@ function Signup() {
     let [name, setName] = useState("");
     let [email, setEmail] = useState("");
     let [password, setPasswrod] = useState("")
-    let [accountType, setAccountType] = useState("")
+    let [isManager, setIsManager] = useState(false)
+    let [managerEmail, setManagerEmail] = useState("")
+    
+const handleCheckboxChange = (event) => {
+    setIsManager(event.target.checked);
+};
+
+const handleSubmit = () => {
+    //handle registration here
+    console.log("user logged in")
+}
 
   return (
     <div>
       <h1>Registration</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Full Name</label><br/>
             <input
             type="text"
@@ -20,7 +30,7 @@ function Signup() {
             onChange={(e => setName(e.target.value))}
             /><br/>
 
-            <label>Email</label>
+            <label>Email</label><br/>
             <input
             type='email'
             id='email'
@@ -29,11 +39,37 @@ function Signup() {
             onChange={(e => setEmail(e.target.value))}
             /><br/>
 
-            {/* <label>Account Type</label>
+            <label>Password</label><br/>
+            <input
+            type='password'
+            id='password'
+            placeholder='Enter your password'
+            value={password}
+            onChange={(e => setPasswrod(e.target.value))}
+            /><br/>
+
+
+            <h3>Account Type</h3><br/>
+            <p>Are you a employye?</p>
             <input 
-            type="radio"
-            value={}
-            /> */}
+            type="checkbox"
+            checked={isManager}
+            onChange={handleCheckboxChange}
+            value={isManager}
+            /><br/>
+            { isManager && (
+                <>
+                 <label>Please enter your manager's email</label><br/>
+                <input
+                    type='email'
+                    value={managerEmail}
+                    onChange={(e) => setManagerEmail(e.target.value)}
+                />
+                </>
+            )}
+
+          
+            <input type="submit"/>
 
         </form>
     </div>
